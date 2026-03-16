ebbea02ee13108520d124f20df620d9638ab2996
Weslley Palhares <weslley_palhares@outlook.com>
2026-03-15 23:17:22 -0300

    fix: Adiciona o @Column na minha classe

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/Service/NInjaRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.CadastroDeNinjas.Ninjas.Controller.Service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NInjaRepository extends JpaRepository<NinjaModel, Long> {
+}
